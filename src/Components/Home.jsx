d02c987c31653ab7d4536ed5d18f401fd5b9e268
nagappan <nagappan08@gmail.com>
2022-04-02 22:19:23 +0530

    Adding dervied atoms and then pushing item to cart

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,46 +1,64 @@
 import React from "react";
 import { atom, useAtom } from "jotai";
 import Product from "./Product";
-import Cart from "./Cart";
 
 export const productAtom = atom(Product);
-export const priceAtom = atom(Cart);
-export const totalAtom = atom(0);
+export const cartAtom = atom([]);
+const totalAtom = atom((get) => {
+  let totalPrice = 0;
+  get(cartAtom).forEach(item => {
+    totalPrice += (item.price * item.quantity);
+  })
+  return totalPrice;
+});
+
 
 export default function Home() {
   return (
     <div>
       <div>
         <h2>Shopping Cart Example</h2>
-        <hr />
+        <br />
         <ProductsContainer />
-        <hr />
+        <br />
         <CartContainer />
       </div>
     </div>
   );
 }
 
-const ProductsContainer = () => {
+
+function ProductsContainer() {
   const [product, setProduct] = useAtom(productAtom);
-  const [price, setPrice] = useAtom(priceAtom);
-  const [total, setTotal] = useAtom(totalAtom);
+  const [cart, setCart] = useAtom(cartAtom);
   const handleClick = (val) => {
     const data = product;
-    data[val.id].quantity = data[val.id].quantity - 1;
-    setProduct([...data]);
-    addProduct(val);
-  };
-  function addProduct(val) {
-    const data = price;
-    data.forEach((res, i) => {
-      if (res.title === val.title && res.price === val.price) {
-        res.quantity = res.quantity + 1;
-        setTotal(total + res.price);
+    console.log("item to add ", val);
+    let tempCart = [...cart];
+    console.log("before cart add", tempCart);
+
+    let isFound = false;
+    tempCart.forEach(item => {
+      if (item.id === val.id) {
+        item.quantity += 1;
+        isFound = true;
       }
     });
-    setPrice(data);
-  }
+
+    if (!isFound) 
+        tempCart.push({id: val.id, title: val.title, price: val.price, quantity: 1});
+
+    setCart(tempCart);
+    console.log("after cart add", cart);
+
+    data[val.id].quantity = data[val.id].quantity - 1;
+    setProduct([...data]);
+    // addProduct(val);
+    console.log('pushed item', cart);
+
+  };
+  
+  
 
   return (
     <>
@@ -61,19 +79,21 @@ const ProductsContainer = () => {
 };
 
 const CartContainer = () => {
-  const [cart] = useAtom(priceAtom);
-  const [total, setTotal] = useAtom(totalAtom);
-  const CheckOut = () => {
+  const [ cart ] = useAtom(cartAtom);
+  const [ totalPrice ] = useAtom(totalAtom);
+  const doCheckout = () => {
+    console.log("total price ", totalPrice);
     cart.forEach((res, i) => {
       if (res.quantity !== 0) {
         res.quantity = 0;
       }
     });
   };
+  
   return (
     <>
       <h3>Your Cart</h3>
-      {cart &&
+      {cart ?
         cart.map((val) => (
           <div key={val.id}>
             {val.quantity !== 0 ? (
@@ -82,15 +102,15 @@ const CartContainer = () => {
               </>
             ) : null}
           </div>
-        ))}
-      Total:$ {total}
+        )) : "Please add items to the cart"}
+      <br />
+      { totalPrice > 0 ? <>Total: ${totalPrice}</>: null}
       <br />
       <br />
       <button
-        disabled={total === 0}
+        disabled={cart.length === 0}
         onClick={() => {
-          CheckOut();
-          setTotal(0);
+          doCheckout();
         }}
       >
         CheckOut
